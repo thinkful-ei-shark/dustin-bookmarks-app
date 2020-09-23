f365b7f0b0e83c4d0dac5ba5620d649cfbf8711f
DUSTIN CHESSNUTT <dustinchesnutt@yahoo.com>
2020-09-23 12:08:51 -0700

    error handleing

--- src/book-mark.js
+++ src/book-mark.js
@@ -3,12 +3,12 @@ function createBookmarkElement(bookmark){
     return `
     <div class="panel panel-default js-bookmark-element" data-item-id="${bookmark.id}">
     <div class="panel-heading" role="tab">
-      <h4 class="panel-title">
+      <h3 class="panel-title">
         <a role="button" data-toggle="collapse" data-parent="#accordion" href="#${bookmark.id}" aria-expanded="true" aria-controls="${bookmark.id}">
           ${bookmark.title}
           <span class="bookmark-rating">${stars(bookmark.rating)}</span>
         </a>
-      </h4>
+      </h3>
     </div>
     <div id="${bookmark.id}" class="panel-collapse collapse" role="tabpanel">
       <div class="panel-body">
@@ -19,6 +19,48 @@ function createBookmarkElement(bookmark){
     </div>
   </div>
     
-    
     `
+}
+
+//create bookmark elements
+function generateBookmarksListString(bookmarkList){
+    const bookmarks =boomarksList.map(bookmark => generateBookmarkElement(bookmark));
+    return bookmarks.join('')
+}
+
+//chessnutt rating
+function chessnutts(numChess){
+    const nutHTML = '<span class ="chessnuttrating"</span>'
+
+    let currentString = '';
+    for (let i = 0; i < numChess; i++){
+        currentString += nutHTML;
+    }
+    return currentString;
+}
+
+//error handling
+function renderError(){
+    if (STORE.error){
+        const err = generateError(STORE.error);
+        $('.error-container').html(err);
+    }else {
+        $('.error-container').empty();
+    }
+}
+
+function closeError(){
+    $('.error-container').on('click','#cancel-error',() =>{
+        STORE.setError(null);
+        renderError();
+    });
+}
+
+function render(){
+    renderError();
+    let bookmarks = STORE.bookmarks.filter(bookmark => {
+        return bookmark.rating >= STORE.minimumRating;
+    });
+
+    $('.js-bookmarks-list').html(generateBookmarksListString(bookmarks));
 }
