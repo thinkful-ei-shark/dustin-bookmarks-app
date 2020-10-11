6a8832df6e0350f41e4cb542263f01dc78068842
Dustin Chessnutt <50764104+Dustin1386@users.noreply.github.com>
2020-10-10 19:51:22 -0700

    Update bookmark.js

--- src/bookmark.js
+++ src/bookmark.js
@@ -1,251 +1,75 @@
-import $ from 'jquery';
-import api from './api'
-import store from './store'
+body{
+    background-color: #C8C8C8 ;
+    display: flex;
+    flex-direction: column;
+}
 
-const deleteBookmarks = api.deleteBookmarks
-//create bookmark html for form
+/* submit button */
+.sub-it{
+    box-shadow: 5px 10px #888888;
+    background-color:#DAF7A6;
+    border: solid black ;
+     border-radius: 15px;
+}
+h1{
+    text-shadow: 2px 2px #888888;
+}
+.form-group{
+    box-shadow: 2px #888888;
+}
 
 
-const generateForm = function(){
-  // if(store.appStarted){
-  //   return ""
-  // }
-  store.appStarted = true
-  return `
-  <div class="container">
-  <header class="banner">
-          <h1>The Greatest Bookmark app I've Ever Created</h1>
-      <form id="js-add-bookmark-form">
-      <div class="row">
-      <div class="col-25">
-        <label for="bookmark-title">Title</label>
-      </div>
-      <div class="col-75">
-      <input type="text" id="bookmark-title" name="bookmark-title" class="form-control js-bookmark-title" placeholder="Enter a title" required aria-label="bookmark-title">
-      </div>
-      </div>
-      <div class="row">
-      <div class="col-25">
-        <label for="bookmark-url">URL ('https://' is required)</label>
-      </div>
-      <div class="col-75">
-        <input type="url" id="bookmark-url" name="bookmark-url" class="form-control js-bookmark-url" placeholder="http(s)://" required aria-label="bookmark-url">
-      </div>
-      </div>
-      <div class="row">
-      <div class="col-25">
-        <label for="bookmark-description">Description</label>
-      </div>
-      <div class="col-75">
-        <input type="text" id="bookmark-description" name="bookmark-description" class="form-control js-bookmark-description" placeholder="Enter a description..." required aria-label="bookmark-description">
-      </div>
-      </div>
-
-      <div class="row">
-      <div class="col-25">
-        <select name="rating" class="js-bookmark-rating" aria-label="rating-dropdown">
-          <option value="1">1-Chessnutt</option>
-          <option value="2">2-Chessnutts</option>
-          <option value="3">3-Chessnutts</option>
-          <option value="4">4-Chessnutts</option>
-          <option value="5">5-Chessnutts</option>
-        </select>
-        <div class="enter">
-          <button type="submit" class="sub-it">Submit</button>
-          </div>  
-      </div>
-      <section class="container">
-      <div class="drop">
-        <div>
-          Sort by Chessnuttst:
-          <select name="Rating filter" class="js-bookmark-rating-filter" aria-label="rating-filter-dropdown">
-            <option value="1">1-Chessnutt</option>
-            <option value="2">2-Chessnutts</option>
-            <option value="3">3-Chessnutts</option>
-            <option value="4">4-Chessnutts</option>
-            <option value="5">5-Chessnutts</option>
-          </select>
-        </div>
-      </div>
-    </section>
-
-      </form>
-    
-      </div> 
- 
-
+.container {
+        border-radius: 5px;
+        background-color: #f2f2f2;
+        padding: 20px;
+        text-align: center;
+        box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+}
+input{
+    width: 50%;
+    padding: 12px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    resize: vertical;
+    text-align: center;
+    box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+}
   
-  
-  
-  `
+
+
+.title{
+    text-align: left;
 }
 
+.col-25 {
+    text-align: center;
+    margin-top: 6px;
+    font-weight: bold;
+    margin-top: 11px;
 
+}           
 
-function createBookmarkElement(bookmark){
-    console.log(bookmark)
-    return `
-    
-    
-    <div class=" container  js-bookmark-element" data-item-id="${bookmark.id}">
-    <div class="" role="tab">
-      <h3 class="panel-title">
-        <a href="#" class = "expand-button" bookmark-id = "${bookmark.id}">
-          ${bookmark.title}
-          <i class="bookmark-rating">${chessnutts(bookmark.rating)}</i>
-        </a>
-      </h3>
-    </div>
-    <div id="${bookmark.id}" class="js-bookmark-description${bookmark.expanded?"":" hidden"}" role="tabpanel">
-      <div class="col-25">
-        <p data-id="${bookmark.id}">${(bookmark.desc)}</p>
-        <p><a data-id="${bookmark.id}" target="_blank" href="${bookmark.url}">Visit Site</a></p>
-        <button type="button" class="btn btn-danger js-bookmark-delete" data-id="${bookmark.id}">Delete</button>
-      </div>
-    </div>
-  </div>
-  
-    
-    `
-}
-
-
-
-
-function render(){
-    renderError();
-    let bookmark = store.bookmark.filter(bookmark => {
-        return bookmark.rating >=  store.minimumRating;
-    });
-    let page = generateForm();
-    page +=generateBookmarksListString(bookmark);
-    $('main').html(page);
-    $(`.js-bookmark-rating-filter option[value='${store.minimumRating}']`).prop("selected", true)
-    bindEventListeners();
-    console.log(store.bookmark)
-
-}
-function getItemIdFromElement(bookmark){
-    return $(bookmark)
-    .closest('.js-bookmark-element')
-    .data('item-id');
-}
-
-//create bookmark elements
-function generateBookmarksListString(bookmarkList){
-    const bookmark = bookmarkList.map(bookmark => createBookmarkElement(bookmark));
-    return bookmark.join('')
-}
-
-//chessnutt rating**
-function chessnutts(numChess){
-    const nutHTML = '<i class="fa fa-thumbs-up"></i>'
-    let currentString = '';
-    for (let i = 0; i < numChess; i++){
-        currentString += nutHTML;
-    }
-    return currentString;
-}
-
-//listeners for form submission new submissions
-function handleNewBookmarkSubmit(){
-    $('main').submit(function(event){
-      event.preventDefault();
-      const chessnuttValue = {};
-      chessnuttValue.title = $('.js-bookmark-title').val();
-      chessnuttValue.url = $('.js-bookmark-url').val();
-      chessnuttValue.desc = $('.js-bookmark-description').val();
-      chessnuttValue.rating = $('.js-bookmark-rating').val();
-      $("#js-add-bookmark-form").trigger("reset")
- 
-
-     
-      
-      api.addBookmark(chessnuttValue)
-        .then((newBookmark) => {
-          store.addBookmark(newBookmark);
-          render();
-        })
-        .catch((error) => {
-         store.setError(error.message);
-          renderError();
-        });
-    });
+  /* Clear floats */
+  .row:after {
+    content: "";
+    display: table;
+    clear: both;
+    text-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24); ;
   }
 
-
-
-function removeBookmarkWhenClicked() {
-    $('main').on('click', '.js-bookmark-delete', event => {
-      const id = getItemIdFromElement(event.currentTarget);
-      api.deleteBookmarks(id)
-        .then(() => {
-          store.findAndDelete(id);
-          render();
-        })
-        .catch((error) => {
-          console.log(err);
-          store.setError(error.message);
-          renderError();
-        });
-    });
-  }
-const handleToggleExpand =()=>{
-    $("main").on("click",".expand-button",function(e){
-        e.preventDefault();
-        let id = $(this).attr("bookmark-id")
-        store.findAndToggleExpanded(id)
-        render()
-    })
+.hidden{
+    display: none;
 }
-//error handling
-function renderError(){
-    if (store.error){
-        $('.error-container').html(store.error);
-    }else {
-        $('.error-container').empty();
+
+
+@media screen and (min-width: 600px) {
+    .container{ 
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 20px;
+    margin: 2% 25% 0 25%;
+    text-align: center;
+    box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
     }
-}
-
-function closeError(){
-    $('.error-container').on('click','#cancel-error',() =>{
-        store.setError(null);
-        renderError();
-    });
-}
-
-
-
-
-
-
-//filter the minimum rating
-function minimumRatingHandler(){
-  $('.js-bookmark-rating-filter').on('change', event =>{
-      let rating = $(event.target).val();
-      store.minimumRating = rating;
-      render();
-  });
-}
-
-//bind event listeners 
-function bindEventListeners(){
-    closeError();
-    handleNewBookmarkSubmit();
-    removeBookmarkWhenClicked();
-    minimumRatingHandler();
-    handleToggleExpand();
-   
-   
-   
-
-    
-   
-}
-
-export default {
-    bindEventListeners,
-    render,
-
-
 }
